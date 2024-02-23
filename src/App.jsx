ea97010e41695be685fbf759f35889f3835d35b4
juanexplosions <juanfeperaltaz@gmail.com>
2024-02-22 20:48:43 -0500

    Initial structure has been created

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+const SquareImage = () => {
+  
+}
+
+
+function App({catImages}) {
+  const [image, setImage] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <p>logo</p>
+        <button>?</button>
+        <button>restart</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <img src={catImages[0].image} alt="cat" />
+        <button>mew</button>
+        <p>Click to make the magic meme happen</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>
+        <p>Made with PURR by @juanexplosions</p>
+      </div>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import catRed from '../public/images/cat-red.png'
+import catBlue from '../public/images/cat-blue.png'
+
+import App from './App'
+
+const catImages = [{
+        id:1,
+        image: catRed
+    },
+    {
+        id:2,
+        image: catBlue
+    }
+]
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <App catImages={catImages} />
+  )
