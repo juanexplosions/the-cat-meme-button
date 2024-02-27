d5eb194e540cd6a61a19d0ceb25c914bf30e3e6d
juanexplosions <juanfeperaltaz@gmail.com>
2024-02-26 19:03:36 -0500

    style: add CSS styles for footer, header and part of the main zone of cat images

--- src/App.jsx
+++ src/App.jsx
@@ -22,31 +22,33 @@ function App({catImages}) {
 
   const renderImages = () => { 
     if(shownCats.length === 0){
-      return <p>Click to make the magic meme happen</p>
+      return <p className='click-to'>Click to make the magic meme happen</p>
     }else{
       return shownCats.map((cat, index) => {
-        return <img className="catImage" key={index} src={`/images/cat-${cat.image}.png`} alt="Imagen de gato" />
+        return <img className="cat-image" key={index} src={`/images/cat-${cat.image}.png`} alt="Imagen de gato" />
       })  
     }
   }
 
   return (
-    <>
-      <div>
+    <div className='container'>
+      <div className='header'>
         <p>logo</p>
-        <button>?</button>
-        <button>restart</button>
+        <div className='buttons'>
+          <button className='secondary'>?</button>
+          <button className='secondary'>
+            <img className='refresh-button' src='/images/refresh-button.svg' alt='Recargar' />
+          </button>
+        </div>
       </div>
-      <div>
-        <button onClick={handleMew}>MEW</button>
+      <div className='cats-grid'>
+        <button className='primary' onClick={handleMew}>MEW</button>
+        {renderImages()}
       </div>
-      <div>
-      {renderImages()}
+      <div className='footer'>
+        <p>Made with <b>PURR</b> by <a href="https://www.instagram.com/juanexplosions/">@juanexplosions</a></p>
       </div>
-      <div>
-        <p>Made with <b>PURR</b> by @juanexplosions</p>
-      </div>
-    </>
+    </div>
   )
 }
 
