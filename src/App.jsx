7ab7356cc3953859b404dea6ccc121703a3e3178
juanexplosions <juanfeperaltaz@gmail.com>
2024-03-01 13:19:33 -0500

    feat & style: now the web works responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,40 @@
 import { useState, useEffect } from 'react'
-import Modal from './components/modal/modal' 
+import Modal from './components/modal/modal'
+import { useWindowSize } from "@uidotdev/usehooks";
+
 
 function App({catImages}) {
   const [shownCats, setShownCats] = useState([])
   const [prevIndex, setPrevIndex] = useState(0)
   const [maxCats, setMaxCats] = useState(0)
   const [showClickText, setShowClickText] = useState(true)
+
+  const isGridFull = maxCats === shownCats.length;
+  const disableButton = isGridFull;
+  const buttonText = isGridFull ? 'OUT OF CATS' : 'MEW';
+
+  const size = useWindowSize();
+  console.log(size)
+
+  const isMobile = size.width <= 600
+  const logo = isMobile ? '/images/tcmb-logo-mobile.png' : '/images/tcmb-logo.png'  
+
+  useEffect(() => {
+    const grid = document.getElementById('cats-grid');
+
+    const altoGrid = grid.clientHeight;
+    const gridWidth = grid.clientWidth;
+
+    const maxCatsAux = Math.floor(altoGrid / 100) * Math.floor(gridWidth / 100);
+    setMaxCats(maxCatsAux);
+
+    console.log(shownCats.length, maxCatsAux, shownCats.length > maxCatsAux)
+    if(shownCats.length > maxCatsAux){
+      const auxArray = shownCats.slice(0, maxCatsAux)
+      setShownCats(auxArray)
+    }
+
+  }, [size])
 
 
   useEffect(() => {
@@ -17,6 +46,8 @@ function App({catImages}) {
     const maxCatsAux = Math.floor(altoGrid / 100) * Math.floor(gridWidth / 100);
     setMaxCats(maxCatsAux);
   }, []);
+
+  useEffect
 
   const handleMew = () =>{
     let newIndex = 0
@@ -43,25 +74,21 @@ function App({catImages}) {
     console.log(showClickText)
   }
 
-  const renderImages = () => { 
+  const renderImages = () => {
+
     if(shownCats.length === 0){
       console.log('No hay gatos')
     }else{
       return shownCats.map((cat, index) => {
         return <img className="cat-image" key={index} src={`/images/cat-${cat.image}.png`} alt="Imagen de gato" />
-      })  
+      })
     }
   }
-
-
-  const isGridFull = maxCats === shownCats.length;
-  const disableButton = isGridFull;
-  const buttonText = isGridFull ? 'OUT OF CATS' : 'MEW';
 
   return (
     <div className='container'>
       <div className='header'>
-        <img className='logo' src="/images/tcmb-logo.png"/>
+        <img className='logo' src={logo}/>
         <div className='buttons'>
           <Modal />
           <button onClick={handleRefresh} className='secondary'>
@@ -74,11 +101,10 @@ function App({catImages}) {
           <button className='primary' onClick={handleMew} disabled={disableButton}> {buttonText} </button>
           <p className='click-to' style={{ visibility: showClickText ? 'visible' : 'hidden' }}>Click to make the magic meme happen</p>
         </div>
-        
         {renderImages()}
       </div>
       <div className='footer'>
-        <p>Made with <b>PURR</b> by <a href="https://www.instagram.com/juanexplosions/">@juanexplosions</a></p>
+        <p>Made with <b>PURR</b> by <a href="https://www.instagram.com/juanexplosions/" target="_blank">@juanexplosions</a></p>
       </div>
     </div>
   )
--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -7,11 +7,17 @@ export default function Modal() {
 
     const toggleModal = () => setShowModal(!showModal)
 
+    const handleBackgroundClick = (event) => {
+        if (event.target.classList.contains("modal-background")) {
+            toggleModal();
+        }
+    };
+
     return(
         <div className="modal-container">
             <button className="secondary" onClick={toggleModal}>?</button>
             {showModal && (
-                <div className="modal-background" onClick={toggleModal}>
+                <div className="modal-background" onClick={handleBackgroundClick}>
                     <div className="modal-content">
                         <div className="modal-button">
                             <button className="secondary" onClick={toggleModal}>X</button>
@@ -19,8 +25,8 @@ export default function Modal() {
                         <div className="modal-texts">
                             <h2>THE CAT MEME BUTTON</h2>
                             <h3>es un proyecto creado para practicar React.js y CSS.</h3>
-                            <p>Es de código abierto y puedes verlo en el <a href="https://github.com/juanexplosions/the-cat-meme-button">repositorio de GitHub.</a></p>
-                            <p>Creado por <a href="https://www.instagram.com/juanexplosions/">@juanexplosions</a>. <br />
+                            <p>Es de código abierto y puedes verlo en el <a href="https://github.com/juanexplosions/the-cat-meme-button" target="_blank">repositorio de GitHub.</a></p>
+                            <p>Creado por <a href="https://www.instagram.com/juanexplosions/" target="_blank">@juanexplosions</a>. <br />
                             Content creator & web developer</p>
                         </div>
                     </div>
